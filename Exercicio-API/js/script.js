34deeea03a2bfdb28224790c84c7a8c3bb88c4ee
Pedro Medeiros <pedrohenriquelm500@gmail.com>
2019-10-03 17:05:04 -0300

    exercicio de API Conversão de moedas

---
+++ Exercicio-API/js/script.js
@@ -0,0 +1,37 @@
+const $seletorUsuario = document.getElementById('seletor1');
+const $moedaUsuario = document.getElementById('valorMoeda');
+const $seletorMoeda = document.getElementById('seletor2');
+const $resultado = document.getElementById('resultado');
+const $botao = document.getElementById('botao'); 
+
+const converterMoeda = (moedaEscolhida, convertida) =>{
+
+    const url = 'https://api.hgbrasil.com/finance?key=development';
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+    fetch ( proxy + url )
+    .then( res => res.json() )
+    .then( res => conversao(res) )
+
+    const conversao = json =>{
+
+        if(moedaEscolhida == "BRL"){
+            $resultado.value = $moedaUsuario.value * json.results.currencies[convertida].buy.toFixed(2);
+        }
+
+    }
+   
+}
+
+$botao.addEventListener("click", () => converterMoeda($seletorUsuario.value, $seletorMoeda.value));
+
+
+
+
+
+
+
+
+
+
+
