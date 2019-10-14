83ea8fe9ee4e65343383e8f36a46d64992ecb648
Pedro Medeiros <pedrohenriquelm500@gmail.com>
2019-10-14 00:00:40 -0300

    criando o front end para consumir a API TMDB

--- TMDb/js/script.js
+++ TMDb/js/script.js
@@ -4,8 +4,28 @@ const url = "https://api.themoviedb.org/3/movie/157336?api_key=42ebeea476d0f2275
 
 
 fetch(url)
-.then(res => res.json())
+.then( res => res.json() )
+.then( res => loadVideos( res ) );
 
-const loadImages = (json) => {
-    json.images
+const loadVideos = ( json ) => {
+    const arrVid = [];
+
+    for ( let i = 0 ; i <= 8; i++){
+        arrVid.push( json.videos.results[i]);
+    }
+    arrVid.map(img()).join(" ");
+    console.log(arrVid);
+    return arrVid;
 }
+
+const img = () => "<img />"
+
+const resultado = ( arr, $el ) => {
+
+    const $container = document.querySelector('.container');
+    const $videoContainer = document.createElement('div');
+    $videoContainer.id = "videoContainer";
+}
+
+
+
