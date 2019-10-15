c4956a8d5c5488b17f22407dbcdc78d46c3b9491
Pedro Medeiros <pedrohenriquelm500@gmail.com>
2019-10-15 11:49:23 -0300

    adicionando as imagens dos filmes pesquisados, consumindo API TMdb

--- TMDb/js/script.js
+++ TMDb/js/script.js
@@ -13,33 +13,68 @@ const mostrarFilmes = (pesquisado) => {
     const mostrar = (json) =>{
         const $container = document.querySelector(".containerFilmes");
         const filmes = [" "];
-        const filmesId = [];
+        const filmesImg = [" "];
 
         for(let i = 0; i < json.results.length; i++){
-            filmes.push( json.results[i].title );
-            filmesId.push( json.results[i].id );
+            if(json.results[i].backdrop_path != null){
+                filmes.push( json.results[i].title );
+                filmesImg.push( json.results[i].backdrop_path );
+            }
         }
-        console.log(filmes);
-        console.log(filmesId);
 
-        $container.innerHTML = criarElemento(filmes);
+        console.log(filmes);
+        console.log(filmesImg);
+
+        
+        const criarElemento = (arr) => {
+
+            return arr.reduce((acc, elemento,i)=>
+            `${acc}
+            <div class="caixa">
+                <h2 class="nomeFilme">
+                   ${filmes[i]} 
+                </h2>
+                <img src="https://image.tmdb.org/t/p/original${elemento}" />
+            </div>
+            `
+            );
+        }
+
+        $container.innerHTML = criarElemento(filmesImg, filmes);
     }
 }
-const criarElemento = (arr) => {
-    return arr.reduce((acc, title, i)=>
-    `${acc}
-    <div class="caixa">
-        <h2 id="nomeFilme">
-            ${title}
-        </h2>
-        <img />
-    </div>
-    `
-    );
-}
+
 
 $botao.addEventListener("click", () => mostrarFilmes($pesquisa.value));
 
 
+
+
+
+
+
+// const urlImg = `https://api.themoviedb.org/3/movie/${filmesId}/images?api_key=42ebeea476d0f2275d00448a81e47aa9&language=en-US`;
+// fetch(urlImg)
+// .then(res => res.json())
+// .then(res => mostrarImagens( res ));
+
+// const mostrarImagens = ( json ) => {
+//     const imagensFilme = [];
+//     for( let i = 0; i < json.length; i++){
+//         imagensFilme.push( json[i] );
+//     }
+//     console.log(imagensFilme);
+// } 
+
+// const inserirImagens = (arr) => {
+//     const htmlImg = arr.map( img => `<img src="${img}">` ).join(" ");
+
+//     const $container = document.querySelector(".containerFilmes");
+//     const $images = document.createElement("img");
+//     $images.innerHTML = htmlImg;
+//     $container.insertBefore($images);
+// }
+
+// inserirImagens(filmesImg);
 
 
