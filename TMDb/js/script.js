6d6a9298c8e6fc7e72dc46b92ff6e7dea1bee016
Pedro Medeiros <pedrohenriquelm500@gmail.com>
2019-10-15 00:09:57 -0300

    conectando com a API, nomes funcionando

--- TMDb/js/script.js
+++ TMDb/js/script.js
@@ -1,31 +1,45 @@
 const $mostrar = document.getElementById('mostrar');
+const $pesquisa = document.getElementById('pesquisa');
+const $botao = document.getElementById('btnPesquisar');
 
-const url = "https://api.themoviedb.org/3/movie/157336?api_key=42ebeea476d0f2275d00448a81e47aa9&append_to_response=videos,images";
+const mostrarFilmes = (pesquisado) => {
 
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=42ebeea476d0f2275d00448a81e47aa9&language=en-US&query=${pesquisado}&page=1&include_adult=false`;
+    
+    fetch(url)
+    .then( res => res.json() )
+    .then( res => mostrar( res ) );
 
-fetch(url)
-.then( res => res.json() )
-.then( res => loadVideos( res ) );
+    const mostrar = (json) =>{
+        const $container = document.querySelector(".containerFilmes");
+        const filmes = [" "];
+        const filmesId = [];
 
-const loadVideos = ( json ) => {
-    const arrVid = [];
+        for(let i = 0; i < json.results.length; i++){
+            filmes.push( json.results[i].title );
+            filmesId.push( json.results[i].id );
+        }
+        console.log(filmes);
+        console.log(filmesId);
 
-    for ( let i = 0 ; i <= 8; i++){
-        arrVid.push( json.videos.results[i]);
+        $container.innerHTML = criarElemento(filmes);
     }
-    arrVid.map(img()).join(" ");
-    console.log(arrVid);
-    return arrVid;
+}
+const criarElemento = (arr) => {
+    return arr.reduce((acc, title, i)=>
+    `${acc}
+    <div class="caixa">
+        <h2 id="nomeFilme">
+            ${title}
+        </h2>
+        <img />
+    </div>
+    `
+    );
 }
 
-const img = () => "<img />"
+$botao.addEventListener("click", () => mostrarFilmes($pesquisa.value));
 
-const resultado = ( arr, $el ) => {
-
-    const $container = document.querySelector('.container');
-    const $videoContainer = document.createElement('div');
-    $videoContainer.id = "videoContainer";
-}
 
 
 
