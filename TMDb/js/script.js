61a9d0bff5e01bacedf062f2a698f6e7c6cd0a46
Pedro Medeiros <pedrohenriquelm500@gmail.com>
2019-10-27 14:46:48 -0300

    finalizando o exercicio de consumo de API

--- TMDb/js/script.js
+++ TMDb/js/script.js
@@ -31,8 +31,7 @@ const mostrar = (json) =>{
     const idFilme = [0];
     const rate = [0];
 
-    
-    
+
     info = json.results.filter ( filme => filme.backdrop_path != null && filme.overview != "")
                         .map ( filme => ({
                             'filme': filme.title,
@@ -41,7 +40,7 @@ const mostrar = (json) =>{
                             'lancamento': filme.release_date,
                             'popularidade': filme.vote_average})
                         );
-
+                        
     for(let i = 0; i < json.results.length; i++){
         if(json.results[i].backdrop_path != null && json.results[i].overview != ""){
             filmes.push( json.results[i].title );
@@ -51,6 +50,7 @@ const mostrar = (json) =>{
             idFilme.push( json.results[i].id );
             parseFloat(rate.push( json.results[i].vote_average )); 
         }
+        console.log(rate);
     }
 
     // CONTRÓI TODA ESTRUTURA HTML A PARTIR DO MÉTODO reduce
@@ -72,10 +72,9 @@ const mostrar = (json) =>{
 }
 
 const criarModal = ($el, id ,$fechar) => {
-    // $el.innerHTML += info[id].filme + info[id].imagem;
     $fechar.style = "z-index:999999;";
     $el.style = "z-index:2000;";
-    
+
     $el.innerHTML = `
     <div id="modal">
         <div class="backdrop">
@@ -92,19 +91,19 @@ const criarModal = ($el, id ,$fechar) => {
         </div>
     </div>
     `;
-    // const fecharModal = () => {
-        
-    //     $el.style = 'z-index:-5;';
-    // }
-    // $fechar.addEventListener("click", fecharModal($containerModal));
-
 }
+
 const criarEstrelas = (rate) =>{
-    var estrela = ""
+    var estrela = "";
     for(let i = 0; i < parseInt(rate); i+=2){
         estrela += `<img src="img/star.png">`;
     }
     return estrela;
+}
+
+const fecharModal = ($el, $fechar) => {
+    $el.style = "opacity:0; transition:300ms;";
+    $fechar.style = "z-index:-5;";
 }
 
 
@@ -115,115 +114,10 @@ $container.addEventListener("click", (t) => {
     if(caixaFilme != "containerFilmes" ){
         criarModal($containerModal, caixaFilme, $close);
     }
-
 });
-
-const fecharModal = ($el, $fechar) => {
-    $el.style = "z-index: -5 ;";
-    $fechar.style = "z-index:-5;";
-}
 
 mostrarFilmes("transformers");
 
 $botao.addEventListener("click", () => mostrarFilmes(pesquisa.value));
 
 $close.addEventListener("click", () => fecharModal($containerModal, $close));
-
-
-
-
-
-
-
-
-
-
-
-
-// const mostrarNaModal = (json) =>{
-//     const modalImagem = [" "];
-//     for(let i = 0; i< json.results.length; i++){
-//         modalImagem.push(`https://image.tmdb.org/t/p/original/${json.results[i].backdrop_path}`);
-//     }
-// }
-// const criarElementoModal = ( arr ) => {
-//     return arr.reduce((acc, elemento,i) =>
-//     `${acc}
-//         <div class="backdrop">
-//             <img src="${elemento}" alt="imagem">
-//         </div>
-//         <div class="informacoes">
-//             <h1></h1>
-//         </div>
-//     `
-//     );
-// }
-// $containerModal.innerHTML = criarElementoModal(modalImagem);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*  */
-
-    
-// const insertCaixa = (arr, $elemento) => {
-//     const htmlCaixa = arr.map(div => `<div class="caixa">${div}</div>`);
-
-//     const $containerSlider = document.getElementById('containerSlider');
-//     const $containerCaixa = document.createElement('div');
-//     $containerCaixa.id = "containerFilmes";
-//     $containerCaixa.innerHTML = htmlCaixa;
-
-//     $containerCaixa.insertBefore($containerSlider, $elemento);
-// }
-
-// insertCaixa(filmes, next);
-
-{/* <div class="progresso" style="width:${rate[i] * 10}%;">${rate[i] * 10}%</div> */}
-
-
-
-
-
-//CONTRÓI TODA ESTRUTURA HTML A PARTIR DO MÉTODO reduce
-// const criarElemento = (arr) => {
-//     return arr.reduce((acc, elemento,i)=>
-//     `${acc}
-
-//     <div class="caixa">
-//         <div class="img">
-//             <h2>${elemento}</h2>
-//             <img src="https://image.tmdb.org/t/p/original/${filmesImg[i]}" alt="backdrops">
-//         </div>
-//         <div class="moreInfoNet">
-//             <div class="nomeFilmes">${elemento}</div>
-//             <div class="overview">${overview[i]}</div>
-//             <div class="released">${released[i]}</div>
-//             <div class="barra_pop">
-                
-//             </div>
-//         </div>
-//     </div>
-    
-//     `
-//     );
-// }
